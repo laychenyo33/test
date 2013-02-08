694e5f6027930a80e3f89f0e4281ec9bd42e00b0
lin chun hsin <l7263626@msn.com>
2013-02-08 09:24:40 +0800

    update watermark.php which had a switch to detect using width or height first

--- watermark.php
+++ watermark.php
@@ -11,9 +11,16 @@ if (eregi("150x150", $src)) {
 }
 
 $base = WideImage::load($src);
-$water_height = round($base->getHeight()/4,0);
-$watermark = $watermark->resize(null,$water_height);
-$water_width = $watermark->getWidth();
+$sw=1;//0:高度優先,1:寬度優先
+if($sw){
+    $water_width = round($base->getWidth()*0.8,0);
+    $watermark = $watermark->resize($water_width);
+    $water_height = $watermark->getHeight();
+}else{
+    $water_height = round($base->getHeight()/4,0);
+    $watermark = $watermark->resize(null,$water_height);
+    $water_width = $watermark->getWidth();
+}
 
 $dest_x = round(($base->getWidth() - $water_width)/2,0);
 $dest_y = round(($base->getHeight() - $water_height)/2,0) ;
