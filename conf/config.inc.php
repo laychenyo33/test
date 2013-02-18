7e8c87d575d3613c31848e34bf265262e067bed4
lin chun hsin <l7263626@msn.com>
2013-02-18 22:25:49 +0800

    new defualt config

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -3,18 +3,18 @@ $cms_cfg['debug'] = false;
 /*
 server name
 */
-$cms_cfg['server_name']	= "ipsdemo";
+$cms_cfg['server_name']	= "localhost";
 $cms_cfg['index_page'] = "index.html";
 $cms_cfg["manage_page"] = "index.php";
 
-$cms_cfg['base_root']	= "/";
+$cms_cfg['base_root']	= "/cms/";
 $cms_cfg['base_url']	= "http://".$cms_cfg['server_name'].$cms_cfg['base_root'];
 $cms_cfg['base_css']	= $cms_cfg['base_root']."css/";
 $cms_cfg['base_images']	= $cms_cfg['base_root']."images/";
 $cms_cfg['base_templates']	= "templates/";
 
 //共用檔案路徑
-$cms_cfg['file_root']="/";
+$cms_cfg['file_root']="/cms/";
 $cms_cfg['file_url']	= "http://".$cms_cfg['server_name'].$cms_cfg['file_root'];
 
 /*
