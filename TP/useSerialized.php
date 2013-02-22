933b736522190be9a8a38a63a566257d02e9399d
lin chun hsin <l7263626@msn.com>
2013-02-22 12:00:25 +0800

    update useSerialized.php in temlpatepower

--- TP/useSerialized.php
+++ TP/useSerialized.php
@@ -4,7 +4,7 @@
 include ('./class.TemplatePower.inc.php');
 
 $tpl = new TemplatePower("./tpmain.stpl");
-$tpl->serializedBase()
+$tpl->serializedBase();
 
 //You don't need to use the assignInclude function anymore.
 //If you need to add an include template, add it in the script
@@ -12,9 +12,9 @@ $tpl->serializedBase()
 //to see an example of this script). After running you can 
 //access it in this script.
 
-$tpl->prepare()
+$tpl->prepare();
 
 /* do the things you normally do here */
 
-$tpl->printToScreen()
+$tpl->printToScreen();
 ?>
