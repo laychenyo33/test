fd067395874ea1e767b586f87bd63643897d9378
lin chun hsin <l7263626@msn.com>
2013-07-04 11:23:23 +0800

    update products.php

--- products.php
+++ products.php
@@ -269,7 +269,7 @@ class PRODUCTS{
         //階層
         $func_str="";
         if($row["pc_custom_status"]==1){//自訂頁面
-//                $row["pc_custom"]=preg_replace("/src=\"([^>]+)upload_files/","src=\"".$cms_cfg["file_root"]."upload_files",$row["pc_custom"]);
+                $row["pc_custom"] = $main->content_file_str_replace($row["pc_custom"]);
                 $tpl->newBlock("PRODUCTS_CATE_CUSTOM");
                 $tpl->assign("VALUE_PC_CUSTOM",$row["pc_custom"]);
         }else{
@@ -481,7 +481,7 @@ class PRODUCTS{
             //是否為自訂頁面
             if($row["p_custom_status"]){
                 $tpl->newBlock("PRODUCTS_DETAIL_CUSTOM");
-//                $row["p_custom"]=preg_replace("/src=\"([^>]+)upload_files/","src=\"".$cms_cfg["file_root"]."upload_files",$row["p_custom"]);
+                $row["p_custom"] = $main->content_file_str_replace($row["p_custom"]);
                 $tpl->assign("VALUE_P_CUSTOM",$row["p_custom"]);
             }else{
                 $this->show_style=($row["p_show_style"])?$row["p_show_style"]:1;
