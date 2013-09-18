080f2df16177bfadd11e0b9fbe72c26e01aa692a
chunshin (cygwin) <l7263626@msn.com>
2013-09-18 04:42:56 +0100

    聯絡我們通知信可以透過設定，依需求使用對應語系的標題對照文字

--- contactus.php
+++ contactus.php
@@ -287,6 +287,23 @@ class CONTACTUS{
                     }else{
                         $tpl->assignGlobal( "VALUE_TERM" , $row['st_inquiry_mail']);
                     }
+                    if($cms_cfg['ws_module']['ws_contactus_mail_title_mapto']){
+                        //載入對照語言語系檔
+                        include "lang/eng-utf8.php";
+                        $tpl->assignGlobal(array(
+                                "MSG_COMPANY_NAME"   => sprintf("<br/>(%s)",$TPLMSG['COMPANY_NAME']),
+                                "MSG_CATE"           => sprintf("<br/>(%s)",$TPLMSG['CATE']),
+                                "MSG_ADDRESS"        => sprintf("<br/>(%s)",$TPLMSG['ADDRESS']),
+                                "MSG_TEL"            => sprintf("<br/>(%s)",$TPLMSG['TEL']),
+                                "MSG_FAX"            => sprintf("<br/>(%s)",$TPLMSG['FAX']),
+                                "MSG_CONTENT"        => sprintf("<br/>(%s)",$TPLMSG['CONTENT']),
+                                "MSG_ATTACH_FILES"   => sprintf("<br/>(%s)",$TPLMSG['CONTACT_US_ATTACH_FILES']),
+                                "MSG_CONTACT_PERSON" => sprintf("<br/>(%s)",$TPLMSG['CONTACT_PERSON']),     
+                                "MSG_COUNTRY"        => sprintf("<br/>(%s)",$TPLMSG['COUNTRY']),        
+                                "MSG_PRODUCT_LIST"   => sprintf("<br/>(%s)",$TPLMSG['CONTACTUS_PRODUCT_LIST']),
+                                "MSG_POSITION"       => sprintf("<br/>(%s)",$TPLMSG['CONTACTUS_POSITION']),                        
+                        ));
+                    }
                     $mail_content=$tpl->getOutputContent();
                     $main->ws_mail_send($_SESSION[$cms_cfg['sess_cookie_name']]['sc_email'],$_REQUEST["cu_email"],$mail_content,$TPLMSG['CONTACT_US'],"cu","");
                 }else{
