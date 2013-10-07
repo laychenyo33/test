f501ae25561a72d1a44bfd808484d4b6d0edf4d3
chunshin (cygwin) <l7263626@msn.com>
2013-10-07 03:00:06 +0100

    index的db建構式補上prefix參數

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once("conf/config.inc.php");
 include_once("libs/libs-mysql.php");
 include_once("libs/libs-main.php");
 include_once("lang/".$cms_cfg['language']."-utf8.php");
-$db = new DB($cms_cfg['db_host'],$cms_cfg['db_user'],$cms_cfg['db_password'],$cms_cfg['db_name']);
+$db = new DB($cms_cfg['db_host'],$cms_cfg['db_user'],$cms_cfg['db_password'],$cms_cfg['db_name'],$cms_cfg['tb_prefix']);
 if(empty($_SESSION[$cms_cfg['sess_cookie_name']]['sc_default_front_page'])){
     $sql="select * from ".$cms_cfg['tb_prefix']."_system_config where sc_id='1'";
     $selectrs = $db->query($sql);
