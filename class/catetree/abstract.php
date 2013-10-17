65229cead25ab473eb3e912e344c506ad9dda7ea
chunshin (cygwin) <l7263626@msn.com>
2013-10-17 08:15:45 +0100

    改寫catetree取得資料表名的方式

--- class/catetree/abstract.php
+++ class/catetree/abstract.php
@@ -37,7 +37,7 @@ class catetree_abstract {
         }
     }
     public function get_sub_id($parent=0){
-        $sql = "select ".$this->_formated_column("id").",".$this->_formated_catename()." from ".$this->_cate_table. " where ".$this->_formated_column("status")."='1' and ".$this->_formated_parent(). "='".$parent."' order by ".$this->_formated_column("sort");
+        $sql = "select ".$this->_formated_column("id").",".$this->_formated_catename()." from ".$this->_db->prefix($this->_cate_table). " where ".$this->_formated_column("status")."='1' and ".$this->_formated_parent(). "='".$parent."' order by ".$this->_formated_column("sort");
         $res = $this->_db->query($sql,true);
         if($this->_db->numRows($res)){
             $id_arr = array();
