557dbcf2e248e47d9f337d08114e9d42fc72af92
chunhsin(toshiba) <l7263626@msn.com>
2013-11-27 14:31:49 +0000

    修改dbtable_abstract

--- class/dbtable/abstract.php
+++ class/dbtable/abstract.php
@@ -10,6 +10,8 @@ abstract class Dbtable_Abstract {
     protected $con = array();
     protected $query_resource;
     protected $order_status = array();
+    protected $sort_col;
+    protected $status_col;
     
     public function __construct(DB $db,$prefix) {
         $this->db = $db;
@@ -52,13 +54,16 @@ abstract class Dbtable_Abstract {
     public function report(){
         return $this->db->report();
     }
+    public function affected_rows(){
+        return $this->db->affected_rows();
+    }
     //取得post資料欄位
     protected function _retrieve_cols($post){
         $this->values = array();
+        $this->con = array();
         foreach($post as $k=>$v){
             if(get_magic_quotes_gpc()){
                 $v = stripslashes($v);
-                $post[$k] = $v;
             }
             if(isset($this->post_cols[$k])){
                 if(is_array($v)){
@@ -110,6 +115,14 @@ abstract class Dbtable_Abstract {
         return $sql;
     }
     
+    protected function _mk_delete_sql($con,$limit=null){
+        if($con){
+            $sql = "delete from ".$this->tablename() . " where ".$con;
+            if($limit)$sql.=" limit ".$limit;
+            return $sql;
+        }
+    }
+    
     protected function tablename(){
         return $this->prefix."_".$this->table;
     }
@@ -135,7 +148,7 @@ abstract class Dbtable_Abstract {
     public function update($data,$con){
         $this->_retrieve_cols($data);
         if(is_string($con)){
-            $this->con[] = (array)$con;
+            $this->con = (array)$con;
         }elseif(is_array($con)){
             foreach($con as $c=>$v){
                 $this->con[]=sprintf("`%s`='%s'",$c,$v);
@@ -149,6 +162,66 @@ abstract class Dbtable_Abstract {
         $sql = $this->_mk_insert_sql();
         $this->_query($sql);
     }
+    //由主鍵刪除
+    public function delete($pk){
+        $this->deleteByCon(sprintf("`%s`='%d'",$this->pk,$pk),1);
+}
+    //由指定條件刪除
+    public function deleteByCon($con,$limit=null){
+        $sql = $this->_mk_delete_sql($con,$limit);
+        if($sql){
+            $this->_query($sql);
+        }
+    }
+
+    function get_insert_id(){
+        return $this->db->get_insert_id();
+    }
+    
+    function get_max_sort_value(){
+        $sql = "select max(".$this->sort_col.") from ".$this->tablename();
+        $this->_query($sql);
+        list($sort) = $this->db->fetch_array($this->query_resource,0);
+        return $sort+1;
+    }
+    //給後台批次刪除使用
+    function del($id){
+        $this->delete($id);
+    }
+    //後台變更狀態
+    function status($pk,$value){
+        if($this->status_col){
+            $data[$this->status_col]=$value;
+            $con[$this->pk]=$pk;
+            $this->update($data,$con);
+        }
+    }
+    //後台排序
+    function sort($pk,$values){
+        if($this->sort_col){
+            $data[$this->sort_col]=$values[$pk];
+            $con[$this->pk]=$pk;
+            $this->update($data,$con);
+        }
+    }       
+    //後台複製
+    function copy($pk){
+        global $main;
+        $origin_row = $this->getData($pk)->getDataRow();
+        unset($origin_row[$this->pk]);
+        $keys = array_keys($origin_row);
+        $unique = preg_grep("/seo_filename$/i", $keys);
+        if($unique){
+            foreach($unique as $u){
+                unset($origin_row[$u]);
+            }
+        }
+        $sort = preg_grep("/sort$/i", $keys);
+        if(isset($origin_row[$this->sort_col])){
+            $origin_row[$this->sort_col] = $this->get_max_sort_value();
+        }
+        $this->writeData($origin_row);
+    }    
 }
 
 ?>
