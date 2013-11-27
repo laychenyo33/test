35a7de969b422091f40de0a78ffacb8cfa97cecc
chunhsin(toshiba) <l7263626@msn.com>
2013-11-27 14:28:36 +0000

    新增mainfunc::multiple_select

--- libs/libs-main.php
+++ libs/libs-main.php
@@ -1625,6 +1625,26 @@ class MAINFUNC{
             ));
         }
     }        
+    //multiple select
+    /*參數說明
+     * $blockname，樣版區塊名稱
+     * $datas，所有內容的集合陣列
+     * $values，指定內容的值，值是$datas的索引值，若是陣列，則取第一個值
+     */
+    function multiple_select($blockname,$datas,$values){
+        global $tpl;
+        if(is_array($values)){
+            $values = $values[0];
+        }
+        foreach($datas as $k => $v){
+            $tpl->newBlock(strtoupper($blockname)."_SELECT");
+            $tpl->assign(array(
+                "VALUE_".strtoupper($blockname)."_KEY"  => $k, 
+                "VALUE_".strtoupper($blockname)."_NAME" => $v, 
+                "SELECTED"                   => ($k==$values)?"selected":"",
+            ));
+        }
+    }      
     //參數說明
     /*$datas，所有值的陣列
      *$values，指定值的陣列，內容是上述陣列的索引值範圍
