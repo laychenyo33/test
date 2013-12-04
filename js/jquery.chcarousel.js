4f9ff2a063c66b00ba074dc4cac0f31bf449fa09
chunshin (cygwin) <l7263626@msn.com>
2013-12-04 07:20:12 +0000

    修改jquery.chcarousel.js，不複製元件，取消循環播送

--- js/jquery.chcarousel.js
+++ js/jquery.chcarousel.js
@@ -6,7 +6,6 @@
                 nums:1,   //單一頁的項目
                 toLeftId:'',
                 toRightId:'',
-                recycle:true,
                 pageId:'',
             };
             $.extend(_options,options);
@@ -14,7 +13,7 @@
             var children = $(target).children();
             var nums = children.size();//子項目
             var sw = children.first().outerWidth(true);//單一項目的寬度            
-            var allw = sw*(nums+_options.nums);//總寬度,所有子項目寬度合再加上多餘的顯示項目
+            var allw = sw*nums;//總寬度,所有子項目寬度合
             var gow = sw*_options.nums;//單一頁寬度
             var lid = _options.nums;
             var gid=((nums-(_options.nums+1))<0)?0:nums-(_options.nums+1);
@@ -31,41 +30,25 @@
                         return false;   
                     }       
                     if(curp<pid){
-                        setTimeout(function(){
-                            curp++;
-                            container.data('animating',true);
-                            if(!_options.recycle && curp>=p && _options.toLeftId){
-                                $('#'+_options.toLeftId).hide();
-                            }
-                            if(_options.toRightId)$('#'+_options.toRightId).show();
-                            container.children().filter(":lt("+lid+")").clone(true).appendTo(container);
-                            container.animate({ marginLeft:(0-gow) },1000,function(){
-                                 container.children().filter(":lt("+lid+")").remove();
-                                 $(this).css({ marginLeft:'' }); 
-                                 container.data('animating',false);
-                            });    
-                            if(curp<pid){
-                                setTimeout(arguments.callee,1050);
-                            }
-                        },0);
+                        curp++;
+                        if( curp>=p && _options.toLeftId){
+                            $('#'+_options.toLeftId).hide();
+                        }
+                        container.data('animating',true);
+                        if(_options.toRightId)$('#'+_options.toRightId).show();
+                        container.animate({ marginLeft:"-="+gow },1000,function(){
+                             container.data('animating',false);
+                        });    
                     }else if(curp>pid){
-                        setTimeout(function(){
-                            curp--;
-                            container.data('animating',true);
-                            if(!_options.recycle && curp<=1 && _options.toRightId){
-                                $('#'+_options.toRightId).hide();
-                            }      
-                            if(_options.toLeftId)$('#'+_options.toLeftId).show();
-                            container.children().filter(":gt("+gid+")").clone(true).prependTo(container);
-                            container.css({ marginLeft:(0-gow) });
-                            container.animate({ marginLeft:0 },1000,function(){
-                                container.children().filter(":gt("+(nums-1)+")").remove();
-                                container.data('animating',false);
-                            });  
-                            if(curp>pid){
-                                setTimeout(arguments.callee,1050);
-                            }
-                        },0);
+                        curp--;
+                        container.data('animating',true);
+                        if( curp<=1 && _options.toRightId){
+                            $('#'+_options.toRightId).hide();
+                        }      
+                        if(_options.toLeftId)$('#'+_options.toLeftId).show();
+                        container.animate({ marginLeft:"+="+gow },1000,function(){
+                            container.data('animating',false);
+                        });  
                     }
                 }
             }
@@ -95,15 +78,15 @@
             }
             if(_options.pageId){
                 var ul = $("<ul></ul>");
-                for(var p=1;p<=nums;p++){
-                    var link = $("<a href='#'></a>").attr('rel',p).text(p).click(function(evt){
+                for(var pi=1;pi<=nums;pi++){
+                    var link = $("<a href='#'></a>").attr('rel',pi).text(pi).click(function(evt){
                         evt.preventDefault();
                         var pid = $(this).attr('rel');
                         movingTo(pid);
                         $(this).parents("ul").find("a").removeClass("current");
                         $(this).addClass("current");
                     });
-                    if(curp==p){
+                    if(curp==pi){
                         link.addClass("current");
                     }
                     var li = $("<li></li>");
