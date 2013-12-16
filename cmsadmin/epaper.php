40a5f5bb6df9ea94344ea2007f2a7b9cfbbfb7f5
chunshin (cygwin) <l7263626@msn.com>
2013-12-16 08:26:54 +0000

    後台電子報的會員分類列表會員數一併考慮m_status的狀態

--- cmsadmin/epaper.php
+++ cmsadmin/epaper.php
@@ -470,12 +470,12 @@ class EPAPER{
                 ));
             }
             //列出分類表單
-            $sql="SELECT mc.mc_id, mc_subject, COUNT( m_id ) AS e_subtotal FROM ".$cms_cfg['tb_prefix']."_member AS m LEFT JOIN ".$cms_cfg['tb_prefix']."_member_cate AS mc ON FIND_IN_SET( mc.mc_id, m.mc_id ) GROUP BY mc.mc_id order by mc.mc_id";
+            $sql="SELECT mc.mc_id, mc_subject, sum(if( m_status,1,0) ) AS e_subtotal FROM ".$cms_cfg['tb_prefix']."_member AS m LEFT JOIN ".$cms_cfg['tb_prefix']."_member_cate AS mc ON FIND_IN_SET( mc.mc_id, m.mc_id ) GROUP BY mc.mc_id order by mc.mc_id";
             $selectrs = $db->query($sql);
             $rsnum    = $db->numRows($selectrs);
             while($row = $db->fetch_array($selectrs,1)){
                 //未分類會員不顯示
-                if($row["mc_id"] != NULL){
+                if($row["mc_id"] != NULL && $row['e_subtotal']>0){
                     $tpl->newBlock("MEMBER_CATE");
                     $tpl->assign( array("VALUE_MC_ID"  => $row["mc_id"],
                                         "VALUE_MC_SUBJECT" => $row["mc_subject"],
