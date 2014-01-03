0dcd01ddf09024b82ca91ea4fb89acd6d0e1a2f4
chunshin (cygwin) <l7263626@msn.com>
2014-01-03 03:36:26 +0000

    增加ajax validation驗證的db參數

--- validateUser.php
+++ validateUser.php
@@ -1,7 +1,7 @@
 <?php
 include_once("conf/config.inc.php");
 include_once("libs/libs-mysql.php");
-$db = new DB($cms_cfg['db_host'],$cms_cfg['db_user'],$cms_cfg['db_password'],$cms_cfg['db_name']);
+$db = new DB($cms_cfg['db_host'],$cms_cfg['db_user'],$cms_cfg['db_password'],$cms_cfg['db_name'],$cms_cfg['tb_prefix']);
 /* RECEIVE VALUE */
 $validateValue=$_POST['validateValue'];
 $validateId=$_POST['validateId'];
