2f63338167a160bd89c9f4484f93ff68d26392a7
chunshin (cygwin) <l7263626@msn.com>
2014-02-21 01:00:09 +0000

    後台熱門產品列表刪除產品流程

--- cmsadmin/products.php
+++ cmsadmin/products.php
@@ -1900,7 +1900,7 @@ class PRODUCTS{
             case "del":
                 if($_REQUEST["ws_table"]=="pc"){
                     $this->products_cate_del();
-                }elseif($_REQUEST["ws_table"]=="p" || $_REQUEST["ws_table"]=="p_new"){
+                }elseif($_REQUEST["ws_table"]=="p" || $_REQUEST["ws_table"]=="p_new" || $_REQUEST["ws_table"]=="p_hot" ){
                     $this->products_del();
                 }elseif($_REQUEST["ws_table"]=="pa"){
                     $this->products_application_del();
