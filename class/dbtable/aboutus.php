c83b29901fc9eb3d8f1233b1f352950c7b6198fd
chunhsin(cygwin) <l7263626@msn.com>
2014-03-09 09:29:04 +0100

    移除dbtable_aboutus及dbtable_member裡自定義的

--- class/dbtable/aboutus.php
+++ class/dbtable/aboutus.php
@@ -3,20 +3,7 @@ class Dbtable_Aboutus extends Dbtable_Abstract{
     //可修改的欄位
     protected $table = "aboutus";
     protected $pk = "au_id";
-    protected $post_cols = array(
-        'au_id'              => "",
-        'au_status'          => "",
-        'au_sort'            => "",
-        'au_cate'            => "",
-        'au_subject'         => "",
-        'au_content'         => "",
-        'au_modifydate'      => "", 
-        'au_seo_title'       => "", 
-        'au_seo_keyword'     => "", 
-        'au_seo_description' => "",
-        'au_seo_filename'    => "",
-        'au_seo_h1'          => "",        
-    );     
+    
     protected function _retrieve_cols($post){
         parent::_retrieve_cols($post);
         $au_cate = $post['au_cate_input']?$post['au_cate_input']:$post['au_cate_select'];
