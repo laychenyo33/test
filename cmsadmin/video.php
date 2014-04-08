100ad4d0533e93d0312a0aed22c097a7bf5ffc56
chunhsin <l7263626@msn.com>
2014-04-08 21:42:00 +0800

    update cmsadmin from cms-demo

--- cmsadmin/video.php
+++ cmsadmin/video.php
@@ -29,6 +29,7 @@ class VIDEO{
                 $this->ws_load_tp($this->ws_tpl_file);
                 $tpl->newBlock("JS_MAIN");
                 $tpl->newBlock("JS_FORMVALID");
+                $tpl->newBlock("JS_PREVIEWS_PIC");
                 $this->video_cate_form("add");
                 $this->ws_tpl_type=1;
                 break;
@@ -38,6 +39,7 @@ class VIDEO{
                 $this->ws_load_tp($this->ws_tpl_file);
                 $tpl->newBlock("JS_MAIN");
                 $tpl->newBlock("JS_FORMVALID");
+                $tpl->newBlock("JS_PREVIEWS_PIC");
                 $this->video_cate_form("mod");
                 $this->ws_tpl_type=1;
                 break;
