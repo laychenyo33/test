2fd80a84059beba119dd6f97a78617a1d25a88e3
chunhsin,lin <l7263626@msn.com>
2014-04-15 15:53:55 +0800

    rewrite watermark.php

--- watermark.php
+++ watermark.php
@@ -9,17 +9,32 @@ if (eregi("150x150", $src)) {
 } else {
 	$watermark = WideImage::load('watermark.png');
 }
+//浮水印圖片尺寸
+$origin_width = $watermark->getWidth();
+$origin_height = $watermark->getHeight();
 
 $base = WideImage::load($src);
 $sw=1;//0:高度優先,1:寬度優先
 if($sw){
-    $water_width = round($base->getWidth()*0.8,0);
-    $watermark = $watermark->resize($water_width);
-    $water_height = $watermark->getHeight();
+    $target_width = round($base->getWidth()*0.8,0);
+    if($target_width<$origin_width){
+        $water_width = $target_width;
+        $watermark = $watermark->resize($water_width);
+        $water_height = $watermark->getHeight();
+    }else{
+        $water_width = $origin_width;
+        $water_height = $origin_height;
+    }
 }else{
-    $water_height = round($base->getHeight()/4,0);
-    $watermark = $watermark->resize(null,$water_height);
-    $water_width = $watermark->getWidth();
+    $target_height = round($base->getHeight()/4,0);
+    if($target_height<$origin_height){
+        $water_height = $target_height;
+        $watermark = $watermark->resize(null,$water_height);
+        $water_width = $watermark->getWidth();
+    }else{
+        $water_width = $origin_width;
+        $water_height = $origin_height;
+    }
 }
 
 $dest_x = round(($base->getWidth() - $water_width)/2,0);
