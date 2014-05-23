4a011e5528e77ddc0221b524d6d3b4657fee375c
chunshin (cygwin) <l7263626@msn.com>
2014-05-23 03:36:03 +0100

    課程預覽功能
    Conflicts:
    	cmsadmin/products.php
    	cmsadmin/templates/ws-manage-products-form-tpl.html
    	products.php

--- cmsadmin/products.php
+++ cmsadmin/products.php
@@ -203,7 +203,9 @@ class PRODUCTS{
                 $this->ws_tpl_file = "templates/ws-manage-msg-action-tpl.html";
                 $this->ws_load_tp($this->ws_tpl_file);
                 $this->products_replace();
-                $this->ws_tpl_type=1;
+                if(!$_POST['via_ajax']){
+                    $this->ws_tpl_type=1;
+                }
                 break;
             case "p_del"://產品管理刪除
                 if($_REQUEST["p_id"]!=""){
@@ -990,6 +992,7 @@ class PRODUCTS{
                                           "VALUE_SPEC_TITLE" =>$row["p_spec_title"],
                                           "VALUE_P_CROSS_CATE" => $row["p_cross_cate"],
                                           "VALUE_P_SEO_SHORT_DESC" => $row["p_seo_short_desc"],
+                                          "TAG_PREVIEW_URL" => $cms_cfg['base_root']."products.php?func=p_detail&p_id=".$row['p_id']."&pc_parent=".$row['pc_id']."&preview=1",
                 ));
                 //有排序欄位才重新指定排序值，複製產品不使用原先產品的排序值
                 if($row['p_sort']){
@@ -1024,6 +1027,8 @@ class PRODUCTS{
                 $row2 = $db->fetch_array($selectrs,1);				
                 $_SESSION[$cms_cfg['sess_cookie_name']]["BACK_EDIT_ZONE"]=$_SERVER["HTTP_REFERER"];
                 $pc_id=$row["pc_id"];
+                //預覽連結
+                $tpl->newBlock("PREVIEW_LINK");
             }else{
                 header("location : products.php?func=p_list");
                 die();
@@ -1269,6 +1274,7 @@ class PRODUCTS{
                 $this->p_id=$_REQUEST["now_p_id"];
                 break;
         }
+        $returnJson['code']=0;
         if ( $db_msg == "" ) {
             $p_big_img_replace_str="";
             for($j=1;$j<=$cms_cfg['big_img_limit'];$j++){
