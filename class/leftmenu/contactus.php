a3299604b79f09068b50a32272e76682c641b96c
chunshin (cygwin) <l7263626@msn.com>
2014-07-09 08:07:16 +0100

    leftmenu_*類別

---
+++ class/leftmenu/contactus.php
@@ -0,0 +1,20 @@
+<?php
+class Leftmenu_Contactus extends Leftmenu_Abstract {
+    protected function _getItems(){
+        global $cms_cfg,$ws_array;
+        return array(
+            array(
+                'link' => $cms_cfg['base_root'].'contactus.htm',
+                'name' => $ws_array['main']['contactus'],
+            ),
+            array(
+                'link' => $cms_cfg['base_root'].'faq.htm',
+                'name' => $ws_array['main']['faq'],
+            ),
+        );
+    }
+    
+    protected function _init(){
+        $this->menuItems[0]['tag_cur'] = "current";
+    }
+}
