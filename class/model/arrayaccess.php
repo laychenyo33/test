effd861f764f4340ffbe4ef20588b0461b1daf37
chunshin (cygwin) <l7263626@msn.com>
2014-07-10 02:36:15 +0100

    改寫model_arrayaccess，可以用層次存取，好像真的陣列一樣

--- class/model/arrayaccess.php
+++ class/model/arrayaccess.php
@@ -3,10 +3,13 @@ class Model_Arrayaccess implements arrayaccess {
     protected $_storage;
 
     function &__get($name){
+        if(!isset($this->_storage[$name])){
+            $this->_storage[$name] = new self();
+        }
         return $this->_storage[$name];
     }
     function __set($name,$value){
-        $this->_storage[$name] = $value;
+        $this->_storage[$name] = self::_fill_data($value);
     }
     function __isset($name) {
         return isset($this->_storage[$name]);
@@ -21,9 +24,9 @@ class Model_Arrayaccess implements arrayaccess {
     
     public function offsetSet($offset, $value) {
         if (is_null($offset)) {
-            $this->_storage[] = $value;
+            $this->_storage[] = self::_fill_data($value);
         } else {
-            $this->_storage[$offset] = $value;
+            $this->_storage[$offset] = self::_fill_data($value);
         }
     }
     public function offsetExists($offset) {
@@ -33,6 +36,20 @@ class Model_Arrayaccess implements arrayaccess {
         unset($this->_storage[$offset]);
     }
     public function offsetGet($offset) {
-        return isset($this->_storage[$offset]) ? $this->_storage[$offset] : null;
+        if(!isset($this->_storage[$offset])){
+            $this->_storage[$offset] = new self();
+        }        
+        return $this->_storage[$offset];
     }    
+    static protected function _fill_data($values){
+        if(is_array($values)){
+            $model = new self();
+            foreach($values as $k => $v){
+                $model[$k] = self::_fill_data($v);
+            }
+            return $model;
+        }else{
+            return $values;
+        }
+    }
 }
