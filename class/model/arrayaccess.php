502571adc4aa5d6b465d54b3c9c8f7282fbb326f
chunhsin(cygwin) <l7263626@msn.com>
2014-07-13 14:27:47 +0100

    model_arrayaccess實作IteratorAggregate , ArrayAccess , Serializable , Countable介面

--- class/model/arrayaccess.php
+++ class/model/arrayaccess.php
@@ -1,7 +1,13 @@
 <?php
-class Model_Arrayaccess implements arrayaccess {
+class Model_Arrayaccess implements IteratorAggregate , ArrayAccess , Serializable , Countable  {
     protected $_storage;
 
+    function __construct($array=null) {
+        $this->_storage = array();
+        if(!is_null($array)){
+            $this->_storage = (array)$array;
+        }
+    }
     function &__get($name){
         if(!isset($this->_storage[$name])){
             $this->_storage[$name] = new self();
@@ -17,7 +23,13 @@ class Model_Arrayaccess implements arrayaccess {
     function __unset($name) {
         unset($this->_storage[$name]);
     }
-
+    function __call($name, $arguments) {
+        if(preg_match("/sort$/i", $name) && function_exists($name)){
+            return call_user_func_array($name,array(&$this->_storage,$arguments[0]));
+        }else{
+            throw new Exception('method : '.$name." doesn't exists!");
+        }
+    }
     function isValueExists($name){
         return isset($this->_storage[$name]);
     }
@@ -52,4 +64,47 @@ class Model_Arrayaccess implements arrayaccess {
             return $values;
         }
     }
+    
+    public function getIterator() {
+        return new ArrayIterator($this->_storage);
+    }    
+    
+    public function serialize() {
+        return serialize($this->_storage);
+    }
+    
+    public function unserialize($data) {
+        $this->_storage = unserialize($data);
+    }  
+    
+    public function count() { 
+        return count($this->_storage); 
+    }     
+    public function append($value) {
+        $this->_storage[] = $value;
+    }
+    public function exchangeArray($input){
+        $old_array = $this->_storage;
+        $this->_storage = (array)$input;
+        return $old_array;
+    }
+    //    sort series methods
+//    public function sort($flags = SORT_REGULAR){
+//        return sort($this->_storage,$flags);
+//    }
+//    public function rsort($flags = SORT_REGULAR){
+//        return rsort($this->_storage,$flags);
+//    }
+//    public function ksort($flags = SORT_REGULAR){
+//        return ksort($this->_storage,$flags);
+//    }
+//    public function krsort($flags = SORT_REGULAR){
+//        return krsort($this->_storage,$flags);
+//    }
+//    public function asort($flags = SORT_REGULAR){
+//        return asort($this->_storage,$flags);
+//    }
+//    public function arsort($flags = SORT_REGULAR){
+//        return arsort($this->_storage,$flags);
+//    }
 }
