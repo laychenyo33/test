670d01f372330280c68d02ad55710b9d33cdee6d
Potson <potson@allmarketing.com.tw>
2014-08-25 14:11:50 +0800

    更改圖檔上傳最大限制

--- filemanager/config/config.php
+++ filemanager/config/config.php
@@ -85,8 +85,8 @@ $convert_spaces  = FALSE; //convert all spaces on files name and folders name wi
 // set maximum pixel width and/or maximum pixel height for all images
 // If you set a maximum width or height, oversized images are converted to those limits. Images smaller than the limit(s) are unaffected
 // if you don't need a limit set both to 0
-$image_max_width  = 0;
-$image_max_height = 0;
+$image_max_width  = 800;
+$image_max_height = 800;
 
 //Automatic resizing //
 // If you set $image_resizing to TRUE the script converts all uploaded images exactly to image_resizing_width x image_resizing_height dimension
