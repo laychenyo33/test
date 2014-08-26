eaf5e1d6417f24840e53e3c18074b769ace2562d
chunshin (cygwin) <l7263626@msn.com>
2014-08-26 04:29:27 +0100

    改寫leftmenu_abstract

--- class/leftmenu/abstract.php
+++ class/leftmenu/abstract.php
@@ -3,14 +3,6 @@ abstract class Leftmenu_Abstract {
     public $tpl;
     public $currentRow;
     protected $menuItems;
-    protected $subul1 = array(
-        "CATE"    => "<ul class=\"menu_body\">",
-        "SUBCATE" => "<ul id=\"\" class=\"menu_prod_body\">",
-    );
-    protected $subul2 = array(
-        "CATE"    => "</ul>",
-        "SUBCATE" => "</ul>",
-    );    
     
     function __construct(TemplatePower $tpl) {
         $this->tpl = $tpl;
@@ -30,18 +22,19 @@ abstract class Leftmenu_Abstract {
     protected function _make_menu(array $menu_items,$blockname="CATE",$sub=false,$deep=""){
         $deep = $deep?"S".$deep:"SUB";
         $sub_cate_name = $deep."CATE";        
-        foreach($menu_items as $itme){
-            $this->tpl->newBlock( "LEFT_".$blockname."_LIST" );
-            $this->tpl->assign( array( 
-                "VALUE_".$blockname."_NAME" => $itme['name'],
-                "VALUE_".$blockname."_LINK" => $itme['link'],
-                "WRAPPER_CLASS"             => $itme['class'],
-                "TAG_CURRENT_CLASS"         => $itme['tag_cur'],
-            ));        
-            if($itme['sub']){
-                $this->tpl->assign("TAG_".$deep."_UL1",$this->subul1[$blockname]);
-                $this->tpl->assign("TAG_".$deep."_UL2",$this->subul2[$blockname]);
-                $this->_make_menu($itme['sub'],$sub_cate_name,true,$deep);
+        foreach($menu_items as $item){
+            if(is_array($item)){
+                $this->tpl->newBlock( "LEFT_".$blockname."_LIST" );
+                $this->tpl->assign( array( 
+                    "VALUE_".$blockname."_NAME" => $item['name'],
+                    "VALUE_".$blockname."_LINK" => $item['link'],
+                    "WRAPPER_CLASS"             => $item['class'],
+                    "TAG_CURRENT_CLASS"         => $item['tag_cur'],
+                ));        
+                if($item['sub']){
+                    $this->tpl->newBlock("LEFT_".$sub_cate_name.'ZONE');
+                    $this->_make_menu($item['sub'],$sub_cate_name,true,$deep);
+                }
             }
         }
     }
