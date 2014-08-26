bc253409b96abf580e1e972fc0b90e8856fb54ef
chunshin (cygwin) <l7263626@msn.com>
2014-08-26 04:32:08 +0100

    新增leftmenu_catewithitems及leftmenu_products

---
+++ class/leftmenu/products.php
@@ -0,0 +1,30 @@
+<?php
+class Leftmenu_Products extends Leftmenu_Catewithitems {
+    //分類資訊
+    protected $cate = array(
+        'linkType'    => 'productscate',
+        'labelField'    => 'pc_name',
+        'query' => array(
+            'table'     => 'products_cate',
+            'alias'  => 'b',
+            'pkField' => 'pc_id',
+            'parentField' => 'pc_parent',
+            'select' => '*',
+            'condition' => "pc_status='1'",
+            'order' => 'pc_sort',
+        ),
+    );
+    //項目資訊
+    protected $items = array(
+        'linkType'  => 'products',
+        'labelField' => 'p_name',
+        'query' => array(
+            'table'   => 'products',
+            'alias'  => 'a',
+            'cateField' => 'pc_id',
+            'select' => 'a.*,b.pc_seo_filename',
+            'condition' => "p_status='1'",
+            'order' => 'p_sort',
+        ),
+    );
+}
