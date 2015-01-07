8eaf980b54cf39050ca3ac4e7d61da53d4fbae31
chunhsin,lin <l7263626@msn.com>
2015-01-07 13:27:55 +0800

    後台頁面未登入或沒權限時導到後台index.php
    Conflicts:
    	cmsadmin/convert-prod-desc.php
    	cmsadmin/unlockproducts.php

--- cmsadmin/shipprice.php
+++ cmsadmin/shipprice.php
@@ -4,7 +4,7 @@
 session_start();
 include_once("../conf/config.inc.php");
 if(empty($_SESSION[$cms_cfg['sess_cookie_name']]["USER_ACCOUNT"])){
-    header("location: /");
+    header("location: ".$cms_cfg['manage_root']);
     exit;
 }
 include_once("../libs/libs-manage-sysconfig.php");
