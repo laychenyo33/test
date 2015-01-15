199ba5a760851e78fbea3365c3a4396b67f227eb
Potson <potson@allmarketing.com.tw>
2015-01-15 17:32:11 +0800

    於後台 index 下建立系統檢查 class

--- cmsadmin/index.php
+++ cmsadmin/index.php
@@ -10,7 +10,70 @@ if(trim($_SESSION[$cms_cfg['sess_cookie_name']]["USER_ACCOUNT"])==""){
     $tpl->assignInclude( "TOP_MENU", $cms_cfg['manage_top_menu_tpl']);
     $tpl->assignInclude( "MAIN", "templates/ws-manage-index-tpl.html");
     $tpl->prepare();
+	
+	new SYS_CHECK();
     $main->mamage_authority();
     $tpl->printToScreen();
+}
+
+// 系統檢查
+class SYS_CHECK{
+	
+	private static $msg;
+	private static $title;
+		
+	function __construct(){
+		$this->online_check();
+		$this->version_check();
+		$this->seo_check();
+		$this->output();
+	}
+	
+	// 檢查上線狀態
+	private function online_check(){
+		global $cms_cfg;
+		
+		self::$title[] = '上線狀態';
+		if($cms_cfg['ws_online']){
+			self::$msg[] = "目前上線 (ws_online)";
+		}else{
+			self::$msg[] = '目前下線 (ws_online) , <span style="color: red;">注意! 此狀態下 GA 設為關閉</span>';
+		}
+	}
+	
+	// 檢查系統版本
+	private function version_check(){
+		global $cms_cfg;
+		
+		self::$title[] = '系統版本';
+		if(!empty($cms_cfg["ws_module"]["ws_version"])){
+			self::$msg[] = $cms_cfg["ws_module"]["ws_version"].' (ws_version)';
+		}else{
+			self::$msg[] = '<span style="color: red;">未設定!<span>';
+		}
+	}
+	
+	// 檢查 SEO 狀態
+	private function seo_check(){
+		global $cms_cfg;
+		
+		self::$title[] = 'SEO 狀態';
+		self::$msg[] = ($cms_cfg["ws_module"]["ws_seo"])?'啟動中 (ws_seo)':'未啟動 (ws_seo)';
+	}
+	
+	// 檢查輸出
+	private function output(){
+		global $tpl;
+		
+		if(is_array(self::$msg) && is_array(self::$title)){
+			foreach(self::$msg as $key => $msg){
+				$tpl->newBlock("TAG_SYS_CHECK_LIST");
+				$tpl->assign(array(
+					"VALUE_SYS_TITLE" => self::$title[$key],
+					"VALUE_SYS_MSG" => $msg,
+				));
+			}
+		}
+	}
 }
 ?>
