a49ef32207d2060cd6e39922ef3d45a5befd30d3
Potson <potson@allmarketing.com.tw>
2015-01-16 18:00:28 +0800

    創建自動排序

---
+++ libs/libs-sort.php
@@ -0,0 +1,75 @@
+<?php
+
+	# 自動排序調整功能
+	
+	class SORT{
+		private static $pos;
+		private static $tb;
+		private static $prefix;
+		private static $id;
+		private static $date;
+		private static $key;
+		
+		# 初始化
+		function __construct($tb_name,$prefix,$id,$key){
+			global $cms_cfg;
+			
+			self::$pos = $cms_cfg["sort_pos"]; // 排序順序
+			self::$tb = $tb_name; // 資料表
+			self::$prefix = $prefix; // 資料表前綴字 (例：p,pc,nc 等...)
+			self::$id = $id; // 資料 primary key
+			self::$key = $key; // 修改的 sort key
+			
+			$rs_bool = self::get_date();
+			
+			if($rs_bool){
+				switch(self::$prefix){
+					case "p":
+						self::p_row();
+					break;
+					case "pc":
+						//self::pc_row();
+					break;
+				}
+			}else{
+				# 找不到資料
+			}
+		}
+		
+		# 取得資料
+		protected static function get_date(){
+			global $db,$cms_cfg;
+			
+            $sql = "select * from ".self::$tb." where ".self::$prefix."_id = '".self::$id."'";
+            $selectrs = $db->query($sql);
+			$rsnum    = $db->numRows($selectrs);
+			
+			if(!empty($rsnum)){
+            	self::$date = $db->fetch_array($selectrs,1);
+				return true;
+			}else{
+				return false;
+			}
+		}
+		
+		//------------------------------------------------------------
+		#依照不同功能 執行不同的排序篩檢法
+		
+		# 產品排序
+		protected static function p_row(){
+			global $db,$cms_cfg;
+			
+            $sql = "select p_other.* from ".self::$tb." as p 
+            		left join ".self::$tb." as p_other on p.pc_id = p_other.pc_id 
+            		where p_other.p_id != '".self::$date["p_id"]."' and p_other.pc_id = '".self::$date["pc_id"]."' 
+            		order by p_sort ".self::$pos;
+            $selectrs = $db->query($sql);
+			$rsnum    = $db->numRows($selectrs);
+			
+			if(!empty($rsnum)){
+				
+			}
+		}
+	}
+
+?>
