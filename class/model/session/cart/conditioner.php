09be645a0bf2014a348fca6dfa11de9f98dcb386
chunhsin,lin <l7263626@msn.com>
2015-04-02 15:59:13 +0800

    調整model_session_cart_conditioner，初始化加購產品，條件判斷方法拆解

--- class/model/session/cart/conditioner.php
+++ class/model/session/cart/conditioner.php
@@ -22,12 +22,15 @@ class Model_Session_Cart_Conditioner extends Model_Modules
                 }
             }
             //取得產品
-            $sql = "select b.* from ".App::getHelper('db')->prefix("shopping_condition_products")." as a "
+            $sql = "select b.*,c_id from ".App::getHelper('db')->prefix("shopping_condition_products")." as a "
                     . "inner join ".App::getHelper('db')->prefix("products")." as b on a.p_id=b.p_id where c_id='{$row['id']}' and b.p_status='1' and b.onsale='1' ";
             $res2 = App::getHelper('db')->query($sql,true);
             while($p = App::getHelper('db')->fetch_array($res2,1)){
+                $p['p_name'] = $p['p_name']."(加購品)";
                 $p['price'] = $this->condition[$row['id']]['price'];
-                $this->condition[$row['id']]['products'][] = $p;
+                $p['limit'] = $this->condition[$row['id']]['limit'];
+                $p['addPurchase'] = true;
+                $this->condition[$row['id']]['products'][$p['p_id']] = $p;
             }
         }
     }
@@ -36,18 +39,51 @@ class Model_Session_Cart_Conditioner extends Model_Modules
         $cart_products = $this->_model->get_cart_products();
         $cart_info = $this->_model->get_cart_info();
         $additionalPurchaseProducts = array();
-        foreach($this->condition as $c_id=>$cond){
-            foreach($cart_products as $cp){
-                if( ( $cp['amount']>=$cond['quantity'] && in_array($cond['type'],array('cate','product')) || 
-                      $cart_info['subtotal_price']>=$cond['amount'] && $cond['type']=="order" ) &&  
-                    (in_array($cp['pc_id'],(array)$cond['map']['pc_id']) || in_array($cp['p_id'],(array)$cond['map']['p_id']) ) &&  
-                    !$cond['pick'])
-                {
-                    $additionalPurchaseProducts[$cond['id']] = $cond;
-                    $this->condition[$c_id]['pick']=true;
+        if($this->condition){
+            foreach($this->condition as $c_id=>$cond){
+                foreach($cart_products as $cp){
+                    if( $this->isQualified($cp, $cart_info, $cond) ) {
+                        $additionalPurchaseProducts[$cond['id']] = $cond;
+                        $this->condition[$c_id]['pick']=true;
+                    }
                 }
             }
         }
         return $additionalPurchaseProducts;
+    }
+    /*
+         *  $product: 加入購物車的產品
+         *  $subtotal_price: 訂單小計金額
+         *  $condition 加購設定項目
+         *  符合條件的產品，或訂單小計金額符合選項設定
+         */
+    function isQualified($product,$cartInfo,$condition){
+        if(($this->isQualifiedProduct($product, $condition) || 
+            ($cartInfo['subtotal_price']>=$condition['amount'] && $condition['type']=="order")) && 
+           !$condition['pick']){
+            return true;
+        }
+        return false;
+    }
+    /*
+         *  $product: 加入購物車的產品
+         *  $condition 加購設定項目
+         *  產品為套用加購所屬分類或產品， 且購買數大於等於條件數量，
+         */    
+    function isQualifiedProduct($product,$condition){
+        if( $product['amount']>=$condition['quantity'] && in_array($condition['type'],array('cate','product')) && 
+            (in_array($product['pc_id'],(array)$condition['map']['pc_id']) || in_array($product['p_id'],(array)$condition['map']['p_id']) )
+          ){
+            return true;
+        }
+        return false;
+    }
+    
+    function hasAddPurchaseProduct($c_id,$p_id){
+        return isset($this->condition[$c_id]['products'][$p_id]);
+    }
+    
+    function getAddPurchaseProduct($c_id,$p_id){
+        return $this->condition[$c_id]['products'][$p_id];
     }
 }
