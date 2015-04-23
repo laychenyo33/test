2bf5f74f5a5dc1bd179aa4a4f8af771577212017
chunhsin,lin <l7263626@msn.com>
2015-04-23 18:19:53 +0800

    增加conf/ghelper.php裡要匯出的欄位

--- conf/ghelper.php
+++ conf/ghelper.php
@@ -9,6 +9,8 @@ return array(
     'news_cate' => array('nc_subject','nc_seo_title','nc_seo_keyword','nc_seo_description','nc_seo_h1','nc_seo_short_desc'),
     'news' => array('n_subject','n_short','n_content','n_seo_title','n_seo_keyword','n_seo_description','n_seo_h1'),
     'products_cate' => array('pc_name','pc_name_alias','pc_custom','pc_desc','pc_seo_title','pc_seo_keyword','pc_seo_description','pc_seo_short_desc','pc_seo_down_short_desc','pc_seo_h1','pc_seo_h2'),
-    'products' => array('p_name','p_name_alias','p_custom','p_spec_title','p_spec','p_character_title','p_character','p_desc_title','p_desc','p_seo_title','p_seo_keyword','p_seo_description','p_seo_short_desc','p_seo_h1','p_seo_h2'),
+    'products' => array('p_name','p_name_alias','p_custom','p_serial','p_spec_title','p_spec','p_character_title','p_character','p_desc_title','p_desc','p_desc_strip','p_seo_title','p_seo_keyword','p_seo_description','p_seo_short_desc','p_seo_h1','p_seo_h2'),
+    'products_application' => array('pa_name','pa_name_alias','pa_seo_title','pa_seo_keyword','pa_seo_description','pa_seo_short_desc','pa_seo_h1'),
+    'video' => array('v_subject','v_seo_title','v_seo_keyword','v_seo_description','v_seo_h1'),
 );
 
