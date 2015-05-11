8cbe706b273fee0ee4496fb83987987bed7e7c2a
chunhsin <l7263626@msn.com>
2015-05-12 00:24:11 +0800

    修改model_request_link_news::get_link

--- class/model/request/link/news.php
+++ class/model/request/link/news.php
@@ -2,15 +2,19 @@
 class Model_Request_Link_News extends Model_Request_Link{
     //put your code here
     function get_link($row) {
-        if(App::configs()->ws_module->ws_seo){
-            if(trim($row["n_seo_filename"])==""){
-                $link = App::configs()->base_root."news/ndetail-".$row["nc_id"]."-".$row["n_id"].".html";
+        if($row["n_content_type"]==1) {
+            if(App::configs()->ws_module->ws_seo){
+                if(trim($row["n_seo_filename"])==""){
+                    $link = App::configs()->base_root."news/ndetail-".$row["nc_id"]."-".$row["n_id"].".html";
+                }else{
+                    $link = App::configs()->base_root."news/".$row["n_seo_filename"].".html";
+                }            
             }else{
-                $link = App::configs()->base_root."news/".$row["n_seo_filename"].".html";
-            }            
+                $link = App::configs()->base_root."news.php?func=n_show&nc_id=".$row["nc_id"]."&n_id=".$row["n_id"];
+            }  
         }else{
-            $link = App::configs()->base_root."news.php?func=n_show&nc_id=".$row["nc_id"]."&n_id=".$row["n_id"];
-        }  
+            $link = $row["n_url"];
+        }
         return $link;
     }
 }
