c77aeb854d6a1d930c390216ea445b559721a6b1
chunhsin,lin <l7263626@msn.com>
2015-05-13 11:31:30 +0800

    調整新cart登入頁面文字，使用樣版變數

--- cart/index.php
+++ cart/index.php
@@ -449,7 +449,15 @@
 				$_SESSION[$cms_cfg['sess_cookie_name']]["ERROR_MSG"] = "";
 				//清空錯誤訊息
 				$tpl->newBlock("MEMBER_LOGIN_FORM");
-				$tpl->assignGlobal(array("MSG_LOGIN_ACCOUNT" => $TPLMSG["LOGIN_ACCOUNT"], "MSG_LOGIN_PASSWORD" => $TPLMSG["LOGIN_PASSWORD"], "MSG_LOGIN_BTN" => $TPLMSG["LOGIN_BUTTON"], "MSG_FIRST_BTN" => ($_SESSION[$cms_cfg['sess_cookie_name']]["sc_cart_type"])?$TPLMSG['FIRST_S_BTN']:$TPLMSG['FIRST_I_BTN'], "MSG_MEMBER_LOGIN" => $TPLMSG["MEMBER_LOGIN"], "TAG_FS_SHOPPING" => $TPLMSG['FIRST_TIME_SHOPPING'], ));
+				$tpl->assignGlobal(array(
+                                    "MSG_LOGIN_ACCOUNT" => $TPLMSG["LOGIN_ACCOUNT"], 
+                                    "MSG_LOGIN_PASSWORD" => $TPLMSG["LOGIN_PASSWORD"], 
+                                    "MSG_LOGIN_BTN" => $TPLMSG["LOGIN_BUTTON"], 
+                                    "MSG_FIRST_BTN" => ($_SESSION[$cms_cfg['sess_cookie_name']]["sc_cart_type"])?$TPLMSG['FIRST_S_BTN']:$TPLMSG['FIRST_I_BTN'], 
+                                    "MSG_MEMBER_LOGIN" => $TPLMSG["MEMBER_LOGIN"], "TAG_FS_SHOPPING" => $TPLMSG['FIRST_TIME_SHOPPING'], 
+                                    'TAG_LOGIN_MESSAGE1' => $TPLMSG['CART_LOGIN_MESSAGE1'],
+                                    'TAG_LOGIN_MESSAGE2' => $TPLMSG['CART_LOGIN_MESSAGE2'],
+                                ));
 	
 				//載入驗証碼
 				$main->security_zone();
