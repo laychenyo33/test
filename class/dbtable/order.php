b7a532735b73d4a04f7d7faa2f1467d4713d94a0
chunhsin,lin <l7263626@msn.com>
2015-07-30 10:31:17 +0800

    dbtable_order及dbtable_order_items的delete方法改寫
    Conflicts:
    	class/dbtable/order.php

--- class/dbtable/order.php
+++ class/dbtable/order.php
@@ -4,5 +4,13 @@ class Dbtable_Order extends Dbtable_Abstract{
     protected $table = "order";
     protected $pk = "o_id";
 
+    function delete($pk) {
+        $writeData = array(
+            'o_id' => $pk,
+            'del'   => 1,
+        );
+        $this->writeData($writeData);    
+        App::getHelper('dbtable')->order_items->update(array('del'=>1),"o_id='".$pk."'");
+    }
 }
 ?>
