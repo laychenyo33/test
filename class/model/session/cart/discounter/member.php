3f06b90ed92e6312d48f61de79cc8622009afea8
chunhsin,lin <l7263626@msn.com>
2015-09-01 10:08:48 +0800

    調整discounter_member適用多重會員分類

--- class/model/session/cart/discounter/member.php
+++ class/model/session/cart/discounter/member.php
@@ -2,7 +2,14 @@
 class Model_Session_Cart_Discounter_Member extends Model_Session_Cart_Discounter_Abstract_Pre {
     
     function getDiscount(&$products){
-        $discount = $this->_model->getSessionHandler()->MEMBER_DISCOUNT/100;
+        $db = App::getHelper("db");
+        //適用多重會員分類，單一分類也可以
+        $sql = "select min(mc_discount) from ".$db->prefix("member_cate")." as a "
+                . "inner join ".$db->prefix("member")." as b "
+                . "on find_in_set(a.mc_id,b.mc_id) "
+                . "where m_id='".App::getHelper("session")->MEMBER_ID."'";
+        list($discount) = $db->query_firstRow($sql,0);
+        $discount /= 100;
         return $discount;
     }
 }
